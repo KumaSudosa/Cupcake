d9d3c218ccde3af3fe67e10d589a574714cf265b
Michael N. Korsgaard <Michael12.korsgaard@gmail.com>
2019-10-14 10:46:23 +0200

    Added constructor and flow for ShoppingCart, LineItems & Cupcake

--- CupcakeProject/src/main/java/logic/LineItems.java
+++ CupcakeProject/src/main/java/logic/LineItems.java
@@ -2,4 +2,30 @@ package logic;
 
 public class LineItems {
     
+    private Cupcake cupcake;
+    private int amount;
+    private double subTotalPrice;
+
+    public LineItems(int cupcakeToppingID, int cupcakeBottomID, int amount) {
+        this.cupcake = new Cupcake(cupcakeToppingID, cupcakeBottomID);
+        this.amount = amount;
+        subTotalPrice = amount * cupcake.getPrice();
+    }
+
+    public Cupcake getCupcake() {
+        return cupcake;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public double getSubTotalPrice() {
+        return subTotalPrice;
+    }
+    
+    
+    
+    
+    
 }
