d34dd0d52f3108c8a09e7c6e0098f1ab4abb2ad3
Michael N. Korsgaard <Michael12.korsgaard@gmail.com>
2019-10-21 08:48:01 +0200

    update to DB class to correct error on live server from unfound path file

--- CupcakeProject/src/main/java/persistence/DB.java
+++ CupcakeProject/src/main/java/persistence/DB.java
@@ -23,34 +23,47 @@ public class DB {
     public static Connection getConnection() throws SQLException {
 
         // Gather information for connection for the propeties file.
-        /*
-        * FIX: This does not work when run from server, need to change paths, most likely use classLoader to find Classpath and work from there
-        *
-        *
-        try (FileInputStream f = new FileInputStream("src\\main\\java\\persistence\\db.properties")) {
+        String propertiesFilePath = propertiesFilePath();
 
+        try (FileInputStream f = new FileInputStream(propertiesFilePath)) {
             Properties pros = new Properties();
             pros.load(f);
             URL = pros.getProperty("url");
             USER = pros.getProperty("user");
             PASSWORD = pros.getProperty("password");
-
         } catch (IOException ex) {
             Logger.getLogger(DB.class.getName()).log(Level.SEVERE, null, ex);
         }
-         */
-        URL = "jdbc:mysql://68.183.211.81:3306/cupcakeshop";
-        USER = "Marcus";
-        PASSWORD = "Password123";
-        
+
         try {
-            // Make the connection and send return it
             Class.forName(DRIVER);
         } catch (ClassNotFoundException ex) {
             Logger.getLogger(DB.class.getName()).log(Level.SEVERE, null, ex);
         }
+
         conn = DriverManager.getConnection(URL, USER, PASSWORD);
 
         return conn;
+    }
+
+    /**
+     * This code is an abomination unto all that is holy about programming, born by a mix of sickness, suffering, cheap soda and immense anger, and is the most crude method of which the problem of finding the path to the db.properties file could be solved. This would have been so much easier, and better in every way, if it had been solved by the use of a classLoader or ClassPath method, but since we do not know how to use those, smashing the head against the keyboard was the best solution available.
+     *
+     * @author by much pain and suffering, Michael N. Korsgaard
+     * @return a string of the path for the db.properties file.
+     */
+    public static String propertiesFilePath() {
+        String[] classPath = DB.class.getResource("").toString().split("/");
+        String path = "";
+        for (String string : classPath) {
+            if (!string.equals("file:")) {
+                path += string + "\\";
+            }
+            if (string.equals("CupcakeProject")) {
+                break;
+            }
+        }
+        path += "\\src\\main\\java\\persistence\\db.properties";
+        return path;
     }
 }
